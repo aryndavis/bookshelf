1b1d2a43adad5b13c0abf4620a0cfbd0232b8e9b
Aryn Davis <arynlynn1@gmail.com>
2023-03-04 22:50:22 -0600

    playing around with how to represent a book

---
+++ src/Components/book.jsx
@@ -0,0 +1,26 @@
+import './book.css'
+
+const Book = () => {
+  return (
+    <div>
+
+    
+        <div className="book-container">
+        A Book!
+
+        <p>Title: </p>
+        <p>Author:</p>
+        <p>Synopsis:</p>
+        </div>
+        <div className="book-container2">
+        A Book!
+
+        Title: 
+        Author:
+        Synopsis:
+        </div>
+    </div>
+  );
+};
+
+export default Book;
