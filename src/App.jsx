dab9bcf8794d455c135d45c9ee0a76741033ed11
Aryn Davis <arynlynn1@gmail.com>
2023-03-04 22:50:05 -0600

    adding router stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
-import { useState } from 'react'
+import * as React from "react";
+import { useRoutes } from "react-router-dom";
 import './App.css'
+import Home from './routes/home';
+import CreateBook from "./routes/createbook";
+import ShelfPage from "./routes/shelfpage";
+import AboutPage from "./routes/aboutpage";
+import TopNav from "./Components/topNav";
 
 function App() {
 
-  return (
-    <div className="App">
-      <h1> Bookshelf! </h1>       
-    </div>
-  )
+
+  let element = useRoutes([
+    {
+      path: "*",
+      element: <TopNav />,
+      children: [
+        {
+          element: <Home />,
+          index:true
+        },
+        {
+          path: "add",
+          element: <CreateBook />,
+        },
+        { path: "shelf", element: <ShelfPage /> },
+      ],
+    },
+    { path: "about", element: <AboutPage /> },
+  ]);
+
+  return element;
 }
 
 export default App
