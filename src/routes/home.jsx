0167640cc68f67f2e79d249ec92d27d429c647da
Aryn Davis <arynlynn1@gmail.com>
2023-03-04 22:50:51 -0600

    necessary pages

---
+++ src/routes/home.jsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+import SideNav from "../Components/topNav";
+
+const Home = () => {
+  return (
+    <div>
+      <div className = "whole-page">
+        Home Page! 
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/routes/shelfpage.jsx
@@ -0,0 +1,13 @@
+import Book from "../Components/book";
+
+const ShelfPage = () => {
+  return (
+    <div>
+      Actual Bookshelf!
+
+      <Book />
+    </div>
+  );
+};
+
+export default ShelfPage;
